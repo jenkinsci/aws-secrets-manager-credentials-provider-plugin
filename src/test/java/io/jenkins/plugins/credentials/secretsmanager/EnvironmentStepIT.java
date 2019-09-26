2cfdcaebeb95a650260eabd6aaf6b6233736ba9f
Chris Kilding <user@fake>
2019-09-26 11:12:24 +0100

    Clarify what should happen when binding all types of credentials in the  directive

--- src/test/java/io/jenkins/plugins/credentials/secretsmanager/EnvironmentStepIT.java
+++ src/test/java/io/jenkins/plugins/credentials/secretsmanager/EnvironmentStepIT.java
@@ -1,14 +1,19 @@
 package io.jenkins.plugins.credentials.secretsmanager;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.Certificate;
 import java.util.Collections;
 
+import hudson.model.Result;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.credentials.secretsmanager.util.CreateSecretOperation;
+import io.jenkins.plugins.credentials.secretsmanager.util.Crypto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 /**
@@ -32,7 +37,7 @@ public class EnvironmentStepIT extends AbstractPluginIT implements CredentialTyp
                 "        VAR = credentials('" + foo.getName() + "')",
                 "      }",
                 "      steps {",
-                "        echo \"Credential: $VAR\"",
+                "        echo \"{variable: $VAR}\"",
                 "      }",
                 "    }",
                 "  }",
@@ -40,7 +45,7 @@ public class EnvironmentStepIT extends AbstractPluginIT implements CredentialTyp
 
         // Then
         assertSoftly(s -> {
-            s.assertThat(result.log).as("Log").contains("Credential: ****");
+            s.assertThat(result.log).as("Log").contains("{variable: ****}");
             s.assertThat(result.result).as("Result").isEqualTo(hudson.model.Result.SUCCESS);
         });
     }
@@ -63,7 +68,7 @@ public class EnvironmentStepIT extends AbstractPluginIT implements CredentialTyp
                 "        FOO = credentials('" + foo.getName() + "')",
                 "      }",
                 "      steps {",
-                "        echo \"Credential: {username: $FOO_USR, password: $FOO_PSW}\"",
+                "        echo \"{variable: $FOO, username: $FOO_USR, password: $FOO_PSW}\"",
                 "      }",
                 "    }",
                 "  }",
@@ -71,7 +76,7 @@ public class EnvironmentStepIT extends AbstractPluginIT implements CredentialTyp
 
         // Then
         assertSoftly(s -> {
-            s.assertThat(result.log).as("Log").contains("Credential: {username: ****, password: ****}");
+            s.assertThat(result.log).as("Log").contains("{variable: ****, username: ****, password: ****}");
             s.assertThat(result.result).as("Result").isEqualTo(hudson.model.Result.SUCCESS);
         });
     }
@@ -79,12 +84,71 @@ public class EnvironmentStepIT extends AbstractPluginIT implements CredentialTyp
     @Test
     @ConfiguredWithCode(value = "/integration.yml")
     public void shouldSupportSshPrivateKeyCredentials() {
-        Assertions.fail("TODO implement this test");
+        // Given
+        final CreateSecretOperation.Result foo = createSecret(Fixtures.SSH_PRIVATE_KEY, opts -> {
+            opts.tags = Collections.singletonMap("jenkins:credentials:username", "joe");
+        });
+
+        // When
+        final WorkflowRunResult result = runPipeline("",
+                "pipeline {",
+                "  agent none",
+                "  stages {",
+                "    stage('Example') {",
+                "      environment {",
+                "        FOO = credentials('" + foo.getName() + "')",
+                "      }",
+                "      steps {",
+                "        echo \"{variable: $FOO, username: $FOO_USR, passphrase: $FOO_PSW}\"",
+                "      }",
+                "    }",
+                "  }",
+                "}");
+
+        // Then
+        assertSoftly(s -> {
+            s.assertThat(result.log).as("Log").contains("{variable: ****, username: ****, passphrase: ****}");
+            s.assertThat(result.result).as("Result").isEqualTo(hudson.model.Result.SUCCESS);
+        });
     }
 
+    /**
+     * NOTE: Declarative pipeline (as defined in pipeline-model-definition plugin)  does not support
+     * `environment` bindings for certificate credentials. It should fail if this kind of binding is
+     * attempted.
+     */
     @Test
     @ConfiguredWithCode(value = "/integration.yml")
     public void shouldSupportCertificateCredentials() {
-        Assertions.fail("TODO implement this test");
+        // Given
+        final KeyPair keyPair = Crypto.newKeyPair();
+        final Certificate cert = Crypto.newSelfSignedCertificate(keyPair);
+        final KeyStore keyStore = Crypto.newKeyStore(Fixtures.EMPTY_PASSWORD);
+        try {
+            keyStore.setKeyEntry("test", keyPair.getPrivate(), Fixtures.EMPTY_PASSWORD, new Certificate[]{cert});
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+        // And
+        final CreateSecretOperation.Result foo = createSecret(Crypto.saveKeyStore(keyStore, Fixtures.EMPTY_PASSWORD));
+
+        // When
+        final WorkflowRunResult result = runPipeline("",
+                "pipeline {",
+                "  agent none",
+                "  stages {",
+                "    stage('Example') {",
+                "      environment {",
+                "        FOO = credentials('" + foo.getName() + "')",
+                "      }",
+                "      steps {",
+                "        echo \"{variable: $FOO}\"",
+                "      }",
+                "    }",
+                "  }",
+                "}");
+
+        // Then
+        assertThat(result.result).isEqualTo(Result.FAILURE);
     }
 }
