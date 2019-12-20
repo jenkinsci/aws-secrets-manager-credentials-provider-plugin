e4967ea4083f410ffb63e7b4bdc2d244296a8c35
Mark Haley <mhaley@miovision.com>
2019-12-20 16:22:52 -0500

    Fix StandardCertificateCredentialsIT.shouldSupportSnapshots test.

--- src/main/java/io/jenkins/plugins/credentials/secretsmanager/AwsCredentialsSnapshotTaker.java
+++ src/main/java/io/jenkins/plugins/credentials/secretsmanager/AwsCredentialsSnapshotTaker.java
@@ -1,8 +1,12 @@
 package io.jenkins.plugins.credentials.secretsmanager;
 
+import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
 import com.cloudbees.plugins.credentials.CredentialsSnapshotTaker;
 
+import com.cloudbees.plugins.credentials.common.StandardCertificateCredentials;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import hudson.Extension;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 
 @Extension
 @SuppressWarnings("unused")
@@ -15,6 +19,15 @@ public class AwsCredentialsSnapshotTaker extends CredentialsSnapshotTaker<AwsCre
     @Override
     public AwsCredentials snapshot(AwsCredentials credential) {
         final SecretValue result = credential.getSecretValue();
+
+        if (credential instanceof StringCredentials ||
+                credential instanceof SSHUserPrivateKey ||
+                credential instanceof StandardCertificateCredentials) {
+            return new AwsCredentialsSnapshot(credential.getId(), credential.getDescription(), credential.getTags(), result);
+        } else if (credential instanceof StandardUsernamePasswordCredentials) {
+            return new AwsCredentialsUsernameAndPasswordSnapshot(credential.getId(), credential.getDescription(), credential.getTags(), result);
+        }
+
         return new AwsCredentialsSnapshot(credential.getId(), credential.getDescription(), credential.getTags(), result);
     }
 }
--- src/main/java/io/jenkins/plugins/credentials/secretsmanager/AwsCredentialsUsernameAndPasswordSnapshot.java
+++ src/main/java/io/jenkins/plugins/credentials/secretsmanager/AwsCredentialsUsernameAndPasswordSnapshot.java
@@ -1,24 +1,21 @@
 package io.jenkins.plugins.credentials.secretsmanager;
 
-import java.util.Map;
-
-import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
-import com.cloudbees.plugins.credentials.common.StandardCertificateCredentials;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
-import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+
+import java.util.Map;
 
 /**
  * A self-contained version of the AwsCredentials.
  */
-class AwsCredentialsSnapshot extends AwsCredentials implements StringCredentials, StandardUsernamePasswordCredentials, SSHUserPrivateKey, StandardCertificateCredentials {
+class AwsCredentialsUsernameAndPasswordSnapshot extends AwsCredentials implements StandardUsernamePasswordCredentials {
 
     private static final long serialVersionUID = 1L;
 
     private final SecretValue result;
 
-    AwsCredentialsSnapshot(String id, String description, Map<String, String> tags, SecretValue result) {
+    AwsCredentialsUsernameAndPasswordSnapshot(String id, String description, Map<String, String> tags, SecretValue result) {
         super(id, description, tags);
         this.result = result;
     }
