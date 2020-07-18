5e83cf572fd0325bd68137d8671b1299d24d90ae
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2020-07-18 13:12:17 +0100

    Dependencies: Bump aws-java-sdk to 1.11.821

--- src/test/java/io/jenkins/plugins/credentials/secretsmanager/ListSecretsOperationTest.java
+++ src/test/java/io/jenkins/plugins/credentials/secretsmanager/ListSecretsOperationTest.java
@@ -3,44 +3,7 @@ package io.jenkins.plugins.credentials.secretsmanager;
 import com.amazonaws.AmazonWebServiceRequest;
 import com.amazonaws.ResponseMetadata;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-import com.amazonaws.services.secretsmanager.model.CancelRotateSecretRequest;
-import com.amazonaws.services.secretsmanager.model.CancelRotateSecretResult;
-import com.amazonaws.services.secretsmanager.model.CreateSecretRequest;
-import com.amazonaws.services.secretsmanager.model.CreateSecretResult;
-import com.amazonaws.services.secretsmanager.model.DeleteResourcePolicyRequest;
-import com.amazonaws.services.secretsmanager.model.DeleteResourcePolicyResult;
-import com.amazonaws.services.secretsmanager.model.DeleteSecretRequest;
-import com.amazonaws.services.secretsmanager.model.DeleteSecretResult;
-import com.amazonaws.services.secretsmanager.model.DescribeSecretRequest;
-import com.amazonaws.services.secretsmanager.model.DescribeSecretResult;
-import com.amazonaws.services.secretsmanager.model.GetRandomPasswordRequest;
-import com.amazonaws.services.secretsmanager.model.GetRandomPasswordResult;
-import com.amazonaws.services.secretsmanager.model.GetResourcePolicyRequest;
-import com.amazonaws.services.secretsmanager.model.GetResourcePolicyResult;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
-import com.amazonaws.services.secretsmanager.model.ListSecretVersionIdsRequest;
-import com.amazonaws.services.secretsmanager.model.ListSecretVersionIdsResult;
-import com.amazonaws.services.secretsmanager.model.ListSecretsRequest;
-import com.amazonaws.services.secretsmanager.model.ListSecretsResult;
-import com.amazonaws.services.secretsmanager.model.PutResourcePolicyRequest;
-import com.amazonaws.services.secretsmanager.model.PutResourcePolicyResult;
-import com.amazonaws.services.secretsmanager.model.PutSecretValueRequest;
-import com.amazonaws.services.secretsmanager.model.PutSecretValueResult;
-import com.amazonaws.services.secretsmanager.model.RestoreSecretRequest;
-import com.amazonaws.services.secretsmanager.model.RestoreSecretResult;
-import com.amazonaws.services.secretsmanager.model.RotateSecretRequest;
-import com.amazonaws.services.secretsmanager.model.RotateSecretResult;
-import com.amazonaws.services.secretsmanager.model.SecretListEntry;
-import com.amazonaws.services.secretsmanager.model.Tag;
-import com.amazonaws.services.secretsmanager.model.TagResourceRequest;
-import com.amazonaws.services.secretsmanager.model.TagResourceResult;
-import com.amazonaws.services.secretsmanager.model.UntagResourceRequest;
-import com.amazonaws.services.secretsmanager.model.UntagResourceResult;
-import com.amazonaws.services.secretsmanager.model.UpdateSecretRequest;
-import com.amazonaws.services.secretsmanager.model.UpdateSecretResult;
-import com.amazonaws.services.secretsmanager.model.UpdateSecretVersionStageRequest;
-import com.amazonaws.services.secretsmanager.model.UpdateSecretVersionStageResult;
+import com.amazonaws.services.secretsmanager.model.*;
 
 import org.junit.Test;
 
@@ -175,6 +138,11 @@ public class ListSecretsOperationTest {
 
         @Override
         public UpdateSecretVersionStageResult updateSecretVersionStage(UpdateSecretVersionStageRequest updateSecretVersionStageRequest) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ValidateResourcePolicyResult validateResourcePolicy(ValidateResourcePolicyRequest validateResourcePolicyRequest) {
             throw new UnsupportedOperationException();
         }
 
