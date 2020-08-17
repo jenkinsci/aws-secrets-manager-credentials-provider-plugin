bdd34f8ee38ac71c813a7be8d7443a0a3ac3967f
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2020-08-17 17:00:58 +0100

    Fix Javadoc and code style issues (#38)

--- src/test/java/io/jenkins/plugins/credentials/secretsmanager/supplier/ParallelSupplierTest.java
+++ src/test/java/io/jenkins/plugins/credentials/secretsmanager/supplier/ParallelSupplierTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,28 +15,23 @@ public class ParallelSupplierTest {
     private static final Supplier<Integer> TWO = () -> 2;
     private static final Supplier<Integer> THREE = () -> 3;
 
-    private static <T> Supplier<Collection<T>> newParallelSupplier(Supplier<T>... suppliers) {
-        final Collection<Supplier<T>> supplierCollection = Arrays.asList(suppliers);
-        return new ParallelSupplier<>(supplierCollection);
-    }
-
     @Test
     public void shouldSupplyNothing() {
-        final Supplier<Collection<Integer>> supplier = newParallelSupplier();
+        final Supplier<Collection<Integer>> supplier = new ParallelSupplier<>(Collections.emptyList());
 
         assertThat(supplier.get()).isEmpty();
     }
 
     @Test
     public void shouldSupplyOneThing() {
-        final Supplier<Collection<Integer>> supplier = newParallelSupplier(ONE);
+        final Supplier<Collection<Integer>> supplier = new ParallelSupplier<>(Collections.singletonList(ONE));
 
         assertThat(supplier.get()).containsExactly(1);
     }
 
     @Test
     public void shouldSupplyMultipleThings() {
-        final Supplier<Collection<Integer>> supplier = newParallelSupplier(ONE, TWO, THREE);
+        final Supplier<Collection<Integer>> supplier = new ParallelSupplier<>(Arrays.asList(ONE, TWO, THREE));
 
         assertThat(supplier.get()).containsExactly(1, 2, 3);
     }
